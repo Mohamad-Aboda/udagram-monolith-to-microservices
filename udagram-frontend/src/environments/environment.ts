6ef52e1179066a13324705237653c78718fc0e78
Mohamad-Aboda <MohamedAboda@std.mans.edu.eg>
2022-10-05 20:29:28 +0200

    updated envs

--- udagram-frontend/src/environments/environment.ts
+++ udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://a5d8c65aca22741668fde840a9130c66-1598455077.us-east-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
