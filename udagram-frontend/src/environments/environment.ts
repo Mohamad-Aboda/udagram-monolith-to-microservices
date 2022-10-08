e5ef22db59194a3e0496e42347c2b85529f519a2
Mohamad-Aboda <MohamedAboda@std.mans.edu.eg>
2022-10-08 19:36:22 +0200

    update api host url

--- udagram-frontend/src/environments/environment.ts
+++ udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://a6a61b07a168b41c0b008b856fbd36ec-1006764107.us-east-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
